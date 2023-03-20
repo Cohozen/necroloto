b9a3a3ea667d22d0c1a9a1e69e861eb1fe9ae624
Cohozen <corentin.legal@protonmail.com>
2023-03-20 14:44:38 +0100

    feature: new home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// import necroloto1Img from '../public/necroloto_1.jpg'
+// import necroloto2Img from '../public/necroloto_2.jpg'
+import lotoImg from '../public/loto.jpg'
+
 
 export default function Home() {
   return (
@@ -14,18 +15,41 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description} />
-
-        <div className={styles.center}>
-          <h2 className={inter.className}>Necroloto</h2>
-          <div className={styles.thirteen}>
-            <h4 className={inter.className}>Is Coming</h4>
-          </div>
-        </div>
-
-        <div className={styles.grid} />
-      </main>
+          <section className="h-full flex flex-1 items-center justify-center">
+              <div className="flex flex-col items-center bg-black text-white py-10 lg:py-20">
+                  <div className="container mx-auto flex flex-col md:flex-row items-center my-10 lg:my-20">
+                      <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-6">
+                          <h1 className="text-4xl lg:text-6xl text-yellow-300 pb-4">Necroloto</h1>
+                          <h2 className="text-3xl lg:text-5xl mb-2">Un jeu de parie sur la mort de célébrités</h2>
+                          <p className="text-sm md:text-base text-gray-50 mb-4">
+                              Nécroloteurs, Faites vos prévisions ! <span className="italic">Pour l&apos;amour du jeu.</span>
+                          </p>
+                          <a href="#" className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
+                              Accéder aux paries
+                          </a>
+                      </div>
+                      <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
+                          <div className="h-48 flex flex-wrap content-center inline-block">
+                              {/*<Image*/}
+                              {/*    className="inline-block mt-28 hidden xl:block rounded-full scale-50 rotate-[60deg]"*/}
+                              {/*    src={necroloto1Img}*/}
+                              {/*    alt="Picture of the author"*/}
+                              {/*/>*/}
+                              <Image
+                                  className="inline-block mt-24 md:mt-0 p-8 md:p-0 rounded-full lg:scale-[0.6] rotate-[50deg]"
+                                  src={lotoImg}
+                                  alt="loto picture"
+                              />
+                              {/*<Image*/}
+                              {/*    className="inline-block mt-28 hidden lg:block rounded-full scale-50 rotate-[60deg]"*/}
+                              {/*    src={necroloto2Img}*/}
+                              {/*    alt="Picture of the author"*/}
+                              {/*/>*/}
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
     </>
   )
 }
