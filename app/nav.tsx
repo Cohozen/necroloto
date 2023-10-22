476350873640b7d051ae49525b48f6e75e3b69f8
Cohozen <corentin.legal@protonmail.com>
2023-10-22 15:36:33 +0200

    core: clean code

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,7 +1,5 @@
-import Navbar from './navbar';
-import { getServerSession } from 'next-auth/next';
+import Navbar from "./navbar";
 
 export default async function Nav() {
-  const session = await getServerSession();
-  return <Navbar user={session?.user} />;
+    return <Navbar user={null} />;
 }
