00997b8e161ecc1c9bc2b3ef43f42976366144c2
Cohozen <corentin.legal@protonmail.com>
2023-10-22 15:08:18 +0200

    core: test new template

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    semi: true,
-    singleQuote: false,
-    trailingComma: 'es5',
-    printWidth: 100,
-    tabWidth: 4,
-    useTabs: true,
-    singleAttributePerLine: true,
-}
