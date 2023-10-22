abedc3ec438cf328b570d908a6b1821f171ef2d2
Cohozen <corentin.legal@protonmail.com>
2023-10-22 18:08:47 +0200

    feat(auth): setup clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,23 +3,25 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import Nav from "./nav";
 import { Suspense } from "react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
-    title: "Next.js 13 + PlanetScale + NextAuth + Tailwind CSS",
-    description:
-        "A user admin dashboard configured with Next.js, PlanetScale, NextAuth, Tailwind CSS, TypeScript, ESLint, and Prettier."
+    title: "Necroloto",
+    description: "Necroloto Application."
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className="h-full bg-gray-50">
-            <body className="h-full">
-                <Suspense>
-                    <Nav />
-                </Suspense>
-                {children}
-                <Analytics />
-            </body>
-        </html>
+        <ClerkProvider>
+            <html lang="fr" className="h-full bg-gray-50">
+                <body className="h-full">
+                    <Suspense>
+                        <Nav />
+                    </Suspense>
+                    {children}
+                    <Analytics />
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import { Card, Title, Text } from "@tremor/react";
+import { currentUser } from "@clerk/nextjs";
 
-export const dynamic = "force-dynamic";
+export default async function IndexPage() {
+    const user = await currentUser();
 
-export default async function IndexPage({ searchParams }: { searchParams: { q: string } }) {
+    const buildUserName = () => {
+        if (!user?.firstName && !user?.lastName) return user?.emailAddresses[0]?.emailAddress;
+        if (user?.firstName && !user?.lastName) return user?.firstName;
+        if (!user?.firstName && user?.lastName) return user?.lastName;
+        if (user?.firstName && user?.lastName) return `${user?.firstName} ${user?.lastName}`;
+        return null;
+    };
+
     return (
         <main className="p-4 md:p-10 mx-auto max-w-7xl">
-            <Title>Users</Title>
-            <Text>A list of users retrieved from a MySQL database (PlanetScale).</Text>
-            <Card className="mt-6"></Card>
+            <Title className="text-2xl">
+                Bonjour <span className="font-bold">{buildUserName()}</span>
+            </Title>
+            <Text>Bienvenue sur le Necroloto.</Text>
         </main>
     );
 }
