1cbc0ad95cc9127f4e63b37beb56345f75342e44
Cohozen <corentin.legal@protonmail.com>
2023-11-01 23:19:48 +0100

    feat(api): insert bet route api

---
+++ src/app/api/bets/[id]/route.ts
@@ -0,0 +1,7 @@
+import { getBet, insertBet } from "@/lib/api/bet";
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+    const { id } = params;
+    const result = await getBet(id);
+    return Response.json(result);
+}
--- src/app/api/users/[id]/bet/route.ts
+++ src/app/api/users/[id]/bet/route.ts
@@ -1,7 +1,14 @@
-import { getBetByUser } from "@/lib/api/bet";
+import { getBetByUser, insertBet } from "@/lib/api/bet";
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
     const { id } = params;
     const result = await getBetByUser(id);
+    return Response.json(result);
+}
+
+export async function POST(request: Request, { params }: { params: { id: string } }) {
+    const { id } = params;
+    const body = await request.json();
+    const result = await insertBet(id, body);
     return Response.json(result);
 }
