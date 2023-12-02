a725d22f4dc7147e32fad405d50cc23d3004a06a
Cohozen <corentin.legal@protonmail.com>
2023-12-02 23:01:40 +0100

    feat(database): add develop database

--- src/lib/api/bet.ts
+++ src/lib/api/bet.ts
@@ -18,21 +18,21 @@ export interface Bet {
 
 export async function getBet(id: string): Promise<Bet | null> {
     const client = await clientPromise;
-    const collection = client.db("necroloto").collection("bets");
+    const collection = client.db(process.env.MONGODB_DATABASE).collection("bets");
 
     return await collection.findOne<Bet>({ id }, {});
 }
 
 export async function getBetByUser(userId: string): Promise<Bet | null> {
     const client = await clientPromise;
-    const collection = client.db("necroloto").collection("bets");
+    const collection = client.db(process.env.MONGODB_DATABASE).collection("bets");
 
     return await collection.findOne<Bet>({ userId }, {});
 }
 
 export async function insertBet(userId: string, celebrities: CelebrityBet[]) {
     const client = await clientPromise;
-    const collection = client.db("necroloto").collection("bets");
+    const collection = client.db(process.env.MONGODB_DATABASE).collection("bets");
 
     const newBet: Bet = {
         userId,
