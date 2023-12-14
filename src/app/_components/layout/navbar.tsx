8bef286ab5c11ec22bcd1db9a37135b65ea6e62e
Cohozen <corentin.legal@protonmail.com>
2023-12-14 19:20:31 +0100

    feat(navbar): breadcrumb

--- src/app/_components/layout/navbar.tsx
+++ src/app/_components/layout/navbar.tsx
@@ -2,8 +2,12 @@
 
 import { UserButton } from "@clerk/nextjs";
 import ToggleTheme from "@/components/layout/toggleTheme";
+import { usePathname } from "next/navigation";
+import { is } from "date-fns/locale";
 
 export default function Navbar() {
+    const pathname = usePathname();
+
     return (
         <div className="bg-base-100 text-base-content sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-shadow duration-100 [transform:translate3d(0,0,0)] shadow-sm">
             <div className="navbar w-full">
@@ -25,7 +29,26 @@ export default function Navbar() {
                     </label>
                 </div>
 
-                <div className="flex flex-1 md:gap-1 lg:gap-2"></div>
+                <div className="flex flex-1 md:gap-1 lg:gap-2">
+                    <div className="text-sm breadcrumbs px-2">
+                        <ul>
+                            {pathname.split("/").map((route, index) => {
+                                const isLastRoute = pathname.split("/").length === index + 1;
+                                const href = pathname
+                                    .split("/")
+                                    .slice(0, index + 1)
+                                    .join("/");
+
+                                return (
+                                    <li key={`breadcrumb-${route}`}>
+                                        {!route && <a href="/game">Home</a>}
+                                        {(route && !isLastRoute && <a href={href}>{route}</a>) || route}
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
 
                 <div className="flex-0">
                     <div className="flex-none items-center block mx-2">
