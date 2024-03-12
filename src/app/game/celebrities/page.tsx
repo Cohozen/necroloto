e8943af0b91b14164e8caa4c700339c4e54d8e5c
Cohozen <corentin.legal@protonmail.com>
2024-03-12 18:41:51 +0100

    feat(celebrity): card list celebrities

--- src/app/game/celebrities/page.tsx
+++ src/app/game/celebrities/page.tsx
@@ -1,7 +1,16 @@
-import BuildingPage from "@/components/buildingPage";
+import { Celebrity } from "@prisma/client";
+import { listAllCelebrities } from "@/lib/api/celebrity";
+import CelebritiesCardList from "./celebritiesCardList";
 
-export default function Page({ params }: { params: { id: string } }) {
-    //return <div>Bet from user: {params.id}</div>;
+export default async function Page({ params }: { params: { id: string } }) {
+    const celebrities: Celebrity[] = await listAllCelebrities();
 
-    return <BuildingPage />;
+    return (
+        <main className="p-4 md:p-10 mx-auto max-w-7xl">
+            <div className="flex flex-col gap-4">
+                <h1 className="text-5xl">Toutes les célébrités</h1>
+            </div>
+            {celebrities && <CelebritiesCardList celebrities={celebrities} />}
+        </main>
+    );
 }
