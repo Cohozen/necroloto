4cd8de13c387c88e42e25ce7b97f3c45a1ed1f45
Cohozen <corentin.legal@protonmail.com>
2024-09-28 18:30:06 +0200

    chore(layout): force login et singup page to light theme

--- src/app/(game)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(game)/sign-in/[[...sign-in]]/page.tsx
@@ -2,29 +2,29 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <main className="flex-1 overflow-y-auto flex flex-col items-center justify-center">
+        <main className="light h-screen flex items-center justify-center bg-background">
             <SignIn
                 appearance={{
-                    elements: {
-                        card: "bg-base-100 border-base-300 shadow-none",
-                        headerTitle: "text-base-content font-bold",
-                        headerSubtitle: "text-base-content",
-                        socialButtonsIconButton: "border-base-content hover:bg-base-200",
-                        formFieldLabel: "text-base-content",
-                        formFieldInput: "text-base-content bg-base-100 border-base-content",
-                        formFieldAction: "text-base-content italic text-xs",
-                        formFieldInputShowPasswordButton:
-                            "text-base-content hover:text-base-content/70",
-                        formButtonPrimary:
-                            "bg-primary hover:bg-primary/70 text-sm text-primary-content normal-case",
-                        footerActionLink: "font-bold",
-                        dividerLine: "bg-base-content",
-                        dividerText: "text-base-content",
-                        identityPreview: "border-base-content",
-                        identityPreviewText: "text-base-content",
-                        identityPreviewEditButtonIcon: "text-base-content",
-                        socialButtonsBlockButton: "text-base-content"
-                    }
+                    // elements: {
+                    //     card: "bg-base-100 border-base-300 shadow-none",
+                    //     headerTitle: "text-base-content font-bold",
+                    //     headerSubtitle: "text-base-content",
+                    //     socialButtonsIconButton: "border-base-content hover:bg-base-200",
+                    //     formFieldLabel: "text-base-content",
+                    //     formFieldInput: "text-base-content bg-base-100 border-base-content",
+                    //     formFieldAction: "text-base-content italic text-xs",
+                    //     formFieldInputShowPasswordButton:
+                    //         "text-base-content hover:text-base-content/70",
+                    //     formButtonPrimary:
+                    //         "bg-primary hover:bg-primary/70 text-sm text-primary-content normal-case",
+                    //     footerActionLink: "font-bold",
+                    //     dividerLine: "bg-base-content",
+                    //     dividerText: "text-base-content",
+                    //     identityPreview: "border-base-content",
+                    //     identityPreviewText: "text-base-content",
+                    //     identityPreviewEditButtonIcon: "text-base-content",
+                    //     socialButtonsBlockButton: "text-base-content"
+                    // }
                 }}
             />
         </main>
